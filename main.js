7e57ed65bffcccaedcf32b1992a742c323adc8bb
99Rafick <rafraf9727@gmail.com>
2024-09-05 09:44:16 +0100

    end

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const button = document.querySelector('button')
+const dropdown = document.querySelector('#dropdown')
+const buttonSvg = document.querySelector('#buttonSvg')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+button.addEventListener('click', () => {
+    buttonSvg.classList.toggle('rotate-180');
+    dropdown.classList.toggle('translate-y-3');
+    dropdown.classList.toggle('opacity-0');
+})
